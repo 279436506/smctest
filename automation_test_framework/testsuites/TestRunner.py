bcad9f7f7b9b787d62e4c9ad0ecac739c8eef099
LZHB <279436506@qq.com>
2019-04-10 15:13:08 +0800

    测试报告名称修改

--- automation_test_framework/testsuites/TestRunner.py
+++ automation_test_framework/testsuites/TestRunner.py
@@ -10,17 +10,18 @@ sys.path.append('D:\\smctest')
 
 # 设置报告文件保存路径
 report_path = os.path.dirname(os.path.abspath('.')) + '/test_report/'
+print(report_path)
 # 获取系统当前时间
 now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
 
 # 设置报告名称格式
-HtmlFile = report_path + now + ".html"
+HtmlFile = "result.html"
 fp = open(HtmlFile,"wb")
 case_dir = "D:\\smctest\\automation_test_framework\\testsuites"
 
 suite = unittest.TestLoader().discover(case_dir)
 
-#让git拉取代码
+
 if __name__=='__main__':
     #执行用例
     runner = HTMLTestRunner.HTMLTestReportCN(stream=fp,title=u'数码仓自动化测试报告',description=u'测试情况：')
